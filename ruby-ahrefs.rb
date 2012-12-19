527528fd1d6821fb89e88220c225199fa5da7c3d
Chris Yee <cyee428@gmail.com>
2012-12-18 20:33:21 -0800

    updated .rb

--- ruby-ahrefs.rb
+++ ruby-ahrefs.rb
@@ -1,17 +1,17 @@
 require 'open-uri'
 require 'json'
 
-puts "Target URL" "\t" "Total Backlinks" "\t" "Linking Domains" "\t" "Unique IPs" "\t" ".Com Links" "\t" ".Edu Links" "\t" ".Gov Links" "\t" "Units Left" "\n"
+puts "Target URL \t Total Backlinks \t Linking Domains \t Unique IPs \t .Com Links \t .Edu Links \t .Gov Links \n"
+
+key = "INSERT_YOUR_API_KEY_HERE"
 
 File.open("links.txt").each {|line|
  line = line.gsub("\n","")
  data = line.split("\t")
  target = data.first.strip
 
-key = "INSERT_YOUR_API_KEY_HERE"
 backlink_count = "http://api.ahrefs.com/get_backlinks_count.php?target=#{target}&mode=exact&output=json&AhrefsKey=#{key}"
 domain_count = "http://api.ahrefs.com/get_ref_domains_ips_count.php?target=#{target}&mode=exact&output=json&AhrefsKey=#{key}"
-unit_count = "http://api.ahrefs.com/get_units_left.php?output=json&AhrefsKey=#{key}"
 
 backlinks_data = open(backlink_count).read
 backlinks_results = JSON.parse(backlinks_data)
@@ -19,9 +19,12 @@ backlinks_results = JSON.parse(backlinks_data)
 domain_data = open(domain_count).read
 domain_results = JSON.parse(domain_data)
 
+puts "#{target}" + "\t" + backlinks_results["Result"]["Backlinks"].to_s + "\t" + domain_results["Result"]["Domains"].to_s + "\t" + domain_results["Result"]["Ips"].to_s + "\t" + domain_results["Result"]["ClassC"].to_s + "\t" + domain_results["Result"]["Edu"].to_s + "\t" + domain_results["Result"]["Gov"].to_s + "\n"
+
+}
+
+unit_count = "http://api.ahrefs.com/get_units_left.php?output=json&AhrefsKey=#{key}"
 units_data = open(unit_count).read
 units_results = JSON.parse(units_data)
 
-puts "#{target}" + "\t" + backlinks_results["Result"]["Backlinks"].to_s + "\t" + domain_results["Result"]["Domains"].to_s + "\t" + domain_results["Result"]["Ips"].to_s + "\t" + domain_results["Result"]["ClassC"].to_s + "\t" + domain_results["Result"]["Edu"].to_s + "\t" + domain_results["Result"]["Gov"].to_s + "\t" + units_results["apiresponse"]["api_units_left"].to_s + "\n"
-
-}
+puts "You have " + units_results["AhrefsApiResponse"]["api_units_left"].to_s + " API calls remaining."
